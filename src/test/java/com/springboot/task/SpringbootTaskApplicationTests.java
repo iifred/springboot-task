c9f946f05638cada6aabb6b02457c9a82bd53dc0
iitfred <594703382@qq.com>
2019-12-11 23:16:15 +0800

    1：提交代码

--- src/test/java/com/springboot/task/SpringbootTaskApplicationTests.java
+++ src/test/java/com/springboot/task/SpringbootTaskApplicationTests.java
@@ -1,13 +1,24 @@
 package com.springboot.task;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootTest
 class SpringbootTaskApplicationTests {
 
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
     @Test
     void contextLoads() {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setSubject("今晚开会");
+        simpleMailMessage.setText("时间定于：晚上7:30");
+        simpleMailMessage.setTo("surulzf@163.com");
+        mailSender.send(simpleMailMessage);
     }
 
 }
