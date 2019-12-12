269d868f5a778f413d2e1b7f568a60b46dc0fc1d
iifred <594703382@qq.com>
2019-12-12 11:58:17 +0800

    1：复杂邮件发送模板demo

--- src/test/java/com/springboot/task/SpringbootTaskApplicationTests.java
+++ src/test/java/com/springboot/task/SpringbootTaskApplicationTests.java
@@ -5,6 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @SpringBootTest
 class SpringbootTaskApplicationTests {
@@ -17,8 +23,24 @@ class SpringbootTaskApplicationTests {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setSubject("今晚开会");
         simpleMailMessage.setText("时间定于：晚上7:30");
-        simpleMailMessage.setTo("surulzf@163.com");
+        simpleMailMessage.setTo("lzfsuru@163.com");
+        simpleMailMessage.setFrom("594703382@qq.com");
         mailSender.send(simpleMailMessage);
     }
 
+    @Test
+    void sendMail() throws MessagingException {
+        //创建一个复杂的消息邮件
+        MimeMessage mimeMailMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMailMessage,true);
+        mimeMessageHelper.setSubject("1");
+        mimeMessageHelper.setText("<font style='color:red;'>22</font>",true);
+        mimeMessageHelper.setFrom("594703382@qq.com");
+        mimeMessageHelper.setTo("lzfsuru@163.com");
+
+        //上传文件
+        mimeMessageHelper.addAttachment("1.jpg",new File("C:\\Users\\fred\\Pictures\\1.jpg"));
+
+        mailSender.send(mimeMailMessage);
+    }
 }
